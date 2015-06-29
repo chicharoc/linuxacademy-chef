a4fffacff47133087c7cd2f6d4ecdfc389ed83ab
root <root@chicharoc1.mylabserver.com>
2015-06-29 03:23:14 +0000

    Comit chef-repo for the first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["chicharo2"] = {"site_title" => "Chicharo2 websites coming soon", "port" => 80, "domain" => "chicharoc2.mylabserver.com"}
+default["apache"]["sites"]["chicharob"] = {"site_title" => "Chicharo2 websites coming soon!", "port" => 80, "domain" => "chicharoc2b.mylabserver.com"}
